f628581ece750655fc79f9f7b703dbc7c766dd94
fredli4qooni <fredlifourqoni999@gmail.com>
2025-06-19 23:53:58 +0700

    build: Run ziggy:generate before vite build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
-
+import './ziggy.js';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
